156d402205323b18fd9a8ed27a959c88116d2e93
Adrian <akakiplimo@gmail.com>
2025-04-28 12:50:04 +0300

    export bootstrap as default func

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Validations piped
@@ -52,3 +52,5 @@ export async function bootstrap() {
 }
 
 void bootstrap();
+
+export default bootstrap;
